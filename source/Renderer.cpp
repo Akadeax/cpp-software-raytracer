fdb6698068e0644727644a65562643e004b5ce3b
Patrick Nellessen <patrickqwertz@gmail.com>
2023-09-19 16:03:15 +0200

    W1 Todo7 Todo8

--- source/Renderer.cpp
+++ source/Renderer.cpp
@@ -45,11 +45,14 @@ void Renderer::Render(Scene* pScene) const
 			Ray ray{ camera.origin, rayDir };
 
 			HitRecord closestHit{};
-			pScene->GetClosestHit(ray, closestHit);
+			Plane testPlane{ { 0.f, -50.f, 0.f }, {0.f, 1.f, 0.f }, 0 };
+			GeometryUtils::HitTest_Plane(testPlane, ray, closestHit);
+			//pScene->GetClosestHit(ray, closestHit);
 
 			if (closestHit.didHit)
 			{
-				finalColor = materials[closestHit.materialIndex]->Shade();
+				const float scaledT{ closestHit.t / 500.f };
+				finalColor = { scaledT, scaledT, scaledT };
 			}
 
 			//Update Color in Buffer
